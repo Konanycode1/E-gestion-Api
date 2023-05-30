201e477b897a90d2b8f927dde0ac8289ff8bda1c
nfcdjobo <nfcdjobo@gmail.com>
2023-05-30 14:21:13 +0000

    m

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -7,8 +7,7 @@ const CategorieController = require('../controllers/categorieController');  // O
 const ArticleController = require('../controllers/articleController');  // On importe le controller chargé de faire du CRUD des articles
 const EmployeController = require('../controllers/employeController');  // On importe le controller chargé de faire du CRUD des employés
 const RoleController = require('../controllers/roleController');
-const ArticleSortantController =require('../controllers/articleSortantController')
-
+const ArticleSortantController =require('../controllers/articleSortantController');
 
 // les routes
 Router.get('/', IndexController.dashboard);
@@ -18,7 +17,7 @@ Router.post('/createStocke', StockeController.create);
 Router.post('/createCategorie', CategorieController.create);
 Router.post('/createArticle', ArticleController.create);
 Router.post('/createEmploye', EmployeController.create);
-Router.post('/createArticleSortant', ArticleSortantController.create);    // On définit la route post permettant d'enregistrer un nouveau rôle
+Router.post('/createArticleSortant', ArticleSortantController.create);    // On définit la route post permettant d'enregistrer les articles vendus
 Router.post('/createRole', RoleController.create);    // On définit la route post permettant d'enregistrer un nouveau rôle
 module.exports = Router
 
