dbed841e2610a0587c194ffe29ba552264396c9c
nfcdjobo <nfcdjobo@gmail.com>
2023-05-31 13:57:17 +0000

    mise au point de la route get permettant de récupérer les stocke

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,11 +3,6 @@ module.exports = (req, res, next) => {
     try {
         let token = req.headers.authorization.split(" ")[1];
         let decodeToken = jwt.verify(token, 'RANDOM_TOKEN_KEY');
-<<<<<<< HEAD
-        console.log("deco: ",decodeToken)
-=======
-        
->>>>>>> gestAPI
         const userId =  decodeToken.userId
         console.log("deco: ",userId)
         const status = decodeToken.status
